c9f4215011acc5d3ab50a2bc4f30468da05acf61
MelKori <mieldo@gmail.com>
2020-09-28 22:04:48 +0300

    update project

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject updcon/libpinkas-clj "0.0.4"
+(defproject updcon/libpinkas-clj "0.0.5"
   :description "A Clojure library to support registry operations for microservices"
   :url "https://github.com/updcon/libpinkas-clj"
   :license {:name "MIT"}
-  :dependencies [[http-kit "2.3.0"]
-                 [cheshire "5.9.0"]
-                 [org.clojure/core.async "0.6.532"]]
+  :dependencies [[http-kit "2.5.0"]
+                 [cheshire "5.10.0"]
+                 [org.clojure/core.async "1.3.610"]]
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies [[org.clojure/clojure "1.10.1"]]}})
